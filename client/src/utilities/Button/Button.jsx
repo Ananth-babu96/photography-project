946e5b035babc4f611a1db18cc6baa05e5a9ba8d
ananth <j.ananthrdsm@gmail.com>
2024-10-31 09:38:07 +0530

    remove folder to as as submodule

--- client/src/utilities/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Button.scss";
-const Button = ({ size, text }) => {
-   return <button className={`button ${size}`}>{text}</button>;
-};
-
-export default Button;
