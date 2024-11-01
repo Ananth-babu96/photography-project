42d543c1e32c562107585ae653cde9756027e754
ananth <j.ananthrdsm@gmail.com>
2024-11-01 09:55:33 +0530

    lowercased

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ server.use(express.static("public"));
 
 server.use(
    cors({
-      origin: "https://Ananth-babu96.github.io/photography-project-client",
+      origin: "https://ananth-babu96.github.io/photography-project-client",
    })
 );
 server.use(express.json());
